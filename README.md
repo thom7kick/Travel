abf2af820903b7ac8ba6e1a8917a967993ecada8
thom7kick <tomomot8@gmail.com>
2015-04-04 12:19:18 +0300

    ""

---
+++ README.md
@@ -0,0 +1,1 @@
+# Travel lets talk
---
+++ connect.php
@@ -0,0 +1,9 @@
+<?php
+
+$con = mysqli_connect("localhost","root","yahoo","travel");
+
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+?> 
